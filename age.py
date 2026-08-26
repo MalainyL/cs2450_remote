0b869e6c32fb4be3d482721411d8f17d94214e01
MalainyL <malainy573@hotmail.com>
2026-08-26 13:33:10 -0600

    I made the tone a little sassier

--- age.py
+++ age.py
@@ -2,23 +2,23 @@ import random
 
 
 def main():
-    print("Hello, I am going to guess your age.")
+    print("I know your age. I am going to guess your age.")
     name = input("What is your name?: ")
-    age = 0
+    age = 101
     past_ages = []
     while True:
         age = random.randint(15,40)
         while age in past_ages:
             age = random.randint(15,40)
             if len(past_ages) >= 26:
-                print("You are either too old or too young :(")
+                print("You are either too old or too young to be here :(")
                 return
         answer = input(f"Is your age {age}?: ")
         if answer == 'y':
             break;
         print("Rats.")
         past_ages.append(age)
-    print(f"Your name is {name} and your age is {age}!")
+    print(f"Alas! I have figured it out! \nYour name is {name} and your age is {age}!")
     return
 
 
